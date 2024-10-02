b53fa29ac514351b33bab9e9552ad0c70c41a8ad
JACOB IDOKO <jacobokpoga@gmail.com>
2024-10-01 23:26:15 -0600

    Update skull_strip.sh

--- preprocess/skull_strip.sh
+++ preprocess/skull_strip.sh
@@ -1,27 +1,13 @@
 # !/bin/bash
 
-convert_to_unix_path() {
-    local path=$1
-    # Replace backslashes with forward slashes
-    path=${path//\\//}
-    echo "$path"
-}
-
-convert_to_windows() {
-    local path=$1
-    # Replace forward slashes with backslashes
-    path=${path//\//\\}
-    echo "$path"
-}
-
-INPUT_DIR=$(convert_to_windows "C:\Users\ai2lab\Desktop\ISLES_2024\dataset\preprocessed\images")
+INPUT_DIR="PATH_TO_DIRECTORY_CONTAINING_TIMEPOINTS"
 
 # for input_file in "$INPUT_DIR"/*0002*.nii.gz; do
 for input_file in "$INPUT_DIR"/*0000*.nii.gz; do
     filename=$(basename "$input_file")
 
     #Run the Docker command
-    docker run --gpus all --mount type=bind,source=$(convert_to_unix_path $input_file),target=/$filename \
+    docker run --gpus all --mount type=bind,source=$($input_file),target=/$filename \
                freesurfer/synthstrip:1.6 -i $filename -o $filename
 
 done
